b923ee1def3cfe3559767fd8c38637bcb1bf9062
Marcus Christie <machrist@iu.edu>
2018-07-25 14:08:30 -0400

    Adds live input config panel

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <b-form novalidate>
-            <b-form-group :label="experimentInput.name" :label-for="experimentInput.name" :state="state">
-              <custom-input-editor v-model="experimentInput.value"
-                :id="experimentInput.name"
-                :experiment="experiment"
-                :experiment-input="experimentInput"/>
-              <ul v-if="feedbackMessages && feedbackMessages.length > 1">
-                  <li v-for="feedback in feedbackMessages" :key="feedback">{{ feedback }}</li>
-              </ul>
-              <div v-else-if="feedbackMessages && feedbackMessages.length === 1">
-                  {{ feedbackMessages[0] }}
-              </div>
-            </b-form-group>
-          </b-form>
-        </div>
-      </div>
+      <b-card title="Input Configuration">
+        <b-form-group label="Input Name" label-for="input-name">
+          <b-form-input id="input-name" type="text" v-model="experimentInput.name"
+            :state="nameState"/>
+        </b-form-group>
+        <b-form-group label="User Friendly Description" label-for="input-user-friendly-description">
+          <b-form-input id="input-user-friendly-description" type="text" v-model="experimentInput.userFriendlyDescription"/>
+        </b-form-group>
+        <b-form-checkbox id="input-required" v-model="experimentInput.isRequired">
+          Required?
+        </b-form-checkbox>
+        <b-form-group label="Metadata" label-for="input-metadata" description="Use 'config' to provide configuration values to the input editor, accessible in the input editor as 'editorConfig'.">
+            <b-form-textarea id="input-metadata"
+                     v-model="metadataString"
+                     :rows="4"
+                     :state="metadataState"
+                     @input="metadataUpdated"
+                     @blur.native="metadataString = formatJSON(metadataString)">
+            </b-form-textarea>
+        </b-form-group>
+      </b-card>
+      <b-card title="Input Editor" class="mt-3">
+        <b-form-group :label="experimentInput.name" :label-for="experimentInput.name" :state="state">
+          <custom-input-editor v-model="experimentInput.value"
+            :id="experimentInput.name"
+            :experiment="experiment"
+            :experiment-input="experimentInput"/>
+          <ul v-if="feedbackMessages && feedbackMessages.length > 1">
+              <li v-for="feedback in feedbackMessages" :key="feedback">{{ feedback }}</li>
+          </ul>
+          <div v-else-if="feedbackMessages && feedbackMessages.length === 1">
+              {{ feedbackMessages[0] }}
+          </div>
+        </b-form-group>
+      </b-card>
     </div>
   </div>
 </template>
@@ -37,15 +54,37 @@ export default {
     experimentInput.name = "name1";
     experimentInput.userFriendlyDescription = "Type the value here";
     experimentInput.isRequired = true;
-    experimentInput.metadata = {};
+    experimentInput.metaData = {editor: {config:{}}};
     experimentInput.value = "";
     return {
       experiment: new models.Experiment(),
       experimentInput: experimentInput,
       state: null,
       feedbackMessages: [],
+      metadataString: JSON.stringify(experimentInput.metaData, null, 4),
     }
   },
+  computed: {
+    metadataState: function() {
+      try {
+        JSON.parse(this.metadataString);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+    nameState: function() {
+      return this.experimentInput.name.trim().length > 0;
+    }
+  },
+  methods: {
+    metadataUpdated: function(value) {
+      this.experimentInput.metaData = JSON.parse(value);
+    },
+    formatJSON: function(jsonString) {
+      return JSON.stringify(JSON.parse(jsonString), null, 4);
+    }
+  }
 }
 </script>
 
--- src/components/CustomInputEditor.vue
+++ src/components/CustomInputEditor.vue
@@ -3,6 +3,12 @@
         :placeholder="experimentInput.userFriendlyDescription"
         :state="componentValidState"
         @input="valueChanged"/>
+    <!-- textarea example: shows how to use editorConfig -->
+    <!-- <b-form-textarea :id="id" v-model="data"
+        :placeholder="experimentInput.userFriendlyDescription"
+        :state="componentValidState"
+        :rows="editorConfig.rows"
+        @input="valueChanged"/> -->
 </template>
 
 <script>
@@ -17,5 +23,10 @@ export default {
             required: true,
         },
     },
+    computed: {
+        editorConfig: function() {
+            return this.experimentInput.editorConfig;
+        }
+    }
 }
 </script>
